e9bf69b3bb8ccad571b90087e6d65665b804cf5b
Lauren Miggins <lauren.miggins@gmail.com>
2020-02-20 15:28:03 -0500

    updaing parameters of class

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
+let bubble; 
+
 function setup() {
     createCanvas(600, 400);
-    bubble = new Bubble();
+    // bubble = new Bubble();
 }
 
 function draw() {
     background(0);
-    for (let i = 0; i < 25; i++) {
+    bubble = new Bubble();
+    for (let i = 0; i < 1; i++) {
         bubble.move();
         bubble.show();
         bubble.fill();
@@ -14,22 +17,22 @@ function draw() {
 
 class Bubble {
     constructor() {
-        this.x = 300; 
-        this.y = 200;
-        this.r = random(10, 200);
-        this.h = random(10, 200);
+        this.x = random(width); 
+        this.y = random(height)
+        this.r = random(5, 15);
+        this.h = random(5, 15);
         this.f = random(1, 255);
     }
     fill() {
         this.f;
     }
     move() {
-        this.x = this.x + random(-5, 5);
+        this.x = this.x + random(-5, 5); 
         this.y = this.y + random(-5, 5);
     }
     show() {
-        stroke(255);
-        strokeWeight(4);
+        stroke(this.f);
+        strokeWeight(random(1, 50));
         fill(this.f);
         ellipse(this.x, this.y, this.r, this.h);
     }
